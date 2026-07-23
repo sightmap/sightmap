d92c1eaf3559fb01d7be5ceabc78e7cbaa61b41d
Chip Lay <chiplay@mac.com>
2026-07-23 14:03:45 -0400

    chore(web): remove manual password gate
    Unwrap the home route and delete the PasswordGate component; the landing page no longer sits behind a client-side password.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from '@/pages/Home'
-import PasswordGate from '@/components/PasswordGate'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PasswordGate><Home /></PasswordGate>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
